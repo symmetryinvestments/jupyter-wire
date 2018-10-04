09583de1db1625d4c953a8ebe9abb60538653765
Atila Neves <atila.neves@gmail.com>
2018-10-04 16:33:04 +0200

    struct Kernel

--- source/jupyter/wire/kernel.d
+++ source/jupyter/wire/kernel.d
@@ -13,9 +13,10 @@ import std.typecons: Nullable;
 mixin template Main() {
     int main(string[] args) {
         try {
-            import jupyter.wire.kernel: run;
+            import jupyter.wire.kernel: Kernel;
             const connectionFileName = args[1];
-            run(connectionFileName);
+            auto kernel = Kernel(LanguageInfo(), connectionFileName);
+            kernel.run;
             return 0;
         } catch(Exception e) {
             import std.stdio: stderr;
@@ -30,62 +31,105 @@ mixin template Main() {
 
 }
 
-/**
-   The "real main"
- */
-void run(in string connectionFileName) @safe {
-    import jupyter.wire.connection: fileNameToConnectionInfo, recvRequestMessage;
-    import std.datetime: msecs;
-    import core.thread: Thread;
 
-    const connectionInfo = fileNameToConnectionInfo(connectionFileName);
-    auto sockets = Sockets(connectionInfo);
-
-    for(bool stop; !stop;) {
-        maybeHandleHeartbeat(sockets);
-        const shellShutdown = maybeHandleRequestMessage(sockets, sockets.shell.recvRequestMessage);
-        const controlShutdown = maybeHandleRequestMessage(sockets, sockets.control.recvRequestMessage);
-        stop = shellShutdown || controlShutdown;
-        () @trusted { Thread.sleep(10.msecs); }();
-    }
+struct LanguageInfo {
+    string name;
+    string version_;
+    string fileExtension;
 }
 
 
-bool maybeHandleRequestMessage(ref Sockets sockets, Nullable!Message requestMessage) @safe {
-    if(requestMessage.isNull) return false;
-    return handleRequestMessage(sockets, requestMessage.get);
-}
+struct Kernel {
 
-// returns whether or not to shutdown
-bool handleRequestMessage(ref Sockets sockets, Message requestMessage) @safe {
+    import jupyter.wire.connection: ConnectionInfo, Sockets;
 
-    import jupyter.wire.message: statusMessage, pubMessage;
-    import std.json : JSONValue, parseJSON;
+    private LanguageInfo languageInfo;
+    private Sockets sockets;
+    private int executionCount;
 
-    static int executionCount = 1;
-
-    auto busyMsg = statusMessage(requestMessage.header, "busy");
-    sockets.send(sockets.ioPub, busyMsg);
-
-    scope(exit) {
-        auto idleMsg = statusMessage(requestMessage.header, "idle");
-        sockets.send(sockets.ioPub, idleMsg);
+    this(LanguageInfo languageInfo, in string connectionFileName) @safe {
+        import jupyter.wire.connection: fileNameToConnectionInfo;
+        this(languageInfo, fileNameToConnectionInfo(connectionFileName));
     }
 
-    switch(requestMessage.header.msgType) {
+    this(LanguageInfo languageInfo, ConnectionInfo connectionInfo) @safe {
+        this.languageInfo = languageInfo;
+        this.sockets = Sockets(connectionInfo);
+    }
 
-    default: return false;
+    void run() @safe {
+        import jupyter.wire.connection: recvRequestMessage;
+        import std.datetime: msecs;
+        import core.thread: Thread;
 
-    case "shutdown_request":
+        for(bool stop; !stop;) {
+            maybeHandleHeartbeat(sockets);
+            const shellShutdown = maybeHandleRequestMessage(sockets.shell.recvRequestMessage);
+            const controlShutdown = maybeHandleRequestMessage(sockets.control.recvRequestMessage);
+            stop = shellShutdown || controlShutdown;
+            () @trusted { Thread.sleep(10.msecs); }();
+        }
+    }
+
+    void maybeHandleHeartbeat(ref Sockets sockets) @safe {
+        ubyte[1024] buf;
+        const ret = sockets.heartbeat.tryReceive(buf);
+        const length = ret[0];
+        if(!length) return;
+        sockets.heartbeat.send(buf[0 .. length]);
+    }
+
+    bool maybeHandleRequestMessage(Nullable!Message requestMessage) @safe {
+        if(requestMessage.isNull) return false;
+        return handleRequestMessage(requestMessage.get);
+    }
+
+    // returns whether or not to shutdown
+    bool handleRequestMessage(Message requestMessage) @safe {
+
+        import jupyter.wire.message: statusMessage, pubMessage;
+        import std.json : JSONValue, parseJSON;
+
+        auto busyMsg = statusMessage(requestMessage.header, "busy");
+        sockets.send(sockets.ioPub, busyMsg);
+
+        scope(exit) {
+            auto idleMsg = statusMessage(requestMessage.header, "idle");
+            sockets.send(sockets.ioPub, idleMsg);
+        }
+
+        switch(requestMessage.header.msgType) {
+
+        default: return false;
+
+        case "shutdown_request":
+            return handleShutdown(requestMessage);
+
+        case "kernel_info_request":
+            return handleKernelInfoRequest(requestMessage);
+
+        case "execute_request":
+            return handleExecuteRequest(requestMessage);
+        }
+
+        assert(0);
+    }
+
+
+    bool handleShutdown(Message requestMessage) @safe {
         // TODO: restart
         // The content of the request is just {"restart": bool} so we reuse it
         // for the reply.
         auto replyMessage = Message(requestMessage, "shutdown_reply", requestMessage.content);
         sockets.send(sockets.control, replyMessage);
         return true;
+    }
 
-    case "kernel_info_request":
+    bool handleKernelInfoRequest(Message requestMessage) @safe {
+        import std.json: JSONValue;
+
         JSONValue kernelInfo;
+        kernelInfo["status"] = "ok";
         kernelInfo["protocol_version"] = "5.3.0";
         kernelInfo["implementation"] = "foo";
         kernelInfo["implementation_version"] = "0.0.1";
@@ -99,8 +143,12 @@ bool handleRequestMessage(ref Sockets sockets, Message requestMessage) @safe {
         sockets.send(sockets.shell, replyMessage);
 
         return false;
+    }
 
-    case "execute_request":
+    bool handleExecuteRequest(Message requestMessage) @safe {
+        import jupyter.wire.message: pubMessage;
+        import std.json: JSONValue, parseJSON;
+
         scope(exit) ++executionCount;
 
         {
@@ -141,15 +189,6 @@ bool handleRequestMessage(ref Sockets sockets, Message requestMessage) @safe {
         }
 
         return false;
+
     }
-
-    assert(0);
-}
-
-void maybeHandleHeartbeat(ref Sockets sockets) @safe {
-    ubyte[1024] buf;
-    const ret = sockets.heartbeat.tryReceive(buf);
-    const length = ret[0];
-    if(!length) return;
-    sockets.heartbeat.send(buf[0 .. length]);
 }
