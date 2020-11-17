188f1c8d4f587f86f5d3f1703ebb4c3ccb81c7e5
Joseph C Wang <joequant@gmail.com>
2020-11-17 10:46:20 +0800

    first checkin

---
+++ example/dlang_kernel/source/app.d
@@ -0,0 +1,31 @@
+import jupyter.wire.kernel;
+import jupyter.wire.message : CompleteResult;
+import drepl;
+
+mixin Main!DLangBackend;
+
+struct DLangBackend {
+    enum languageInfo = LanguageInfo("D", "0.0.1", ".d");
+    int value;
+    typeof(interpreter(dmdEngine())) *inpr = null;
+    ExecutionResult execute(in string code, scope IoPubMessageSender sender) {
+        import std.conv: text;
+	if (inpr is null) {
+	   auto inpr_in = interpreter(dmdEngine()); 
+	   inpr = &inpr_in;
+	}
+	auto res = (*inpr).interpret(code);
+	final switch (res.state) with(InterpreterResult.State)
+        {
+        case incomplete:
+            break;
+
+        case success:
+        case error:
+            if (res.stderr.length) return textResult(res.stderr);
+            if (res.stdout.length) return textResult(res.stdout);
+            break;
+        }
+	return textResult("");
+    }
+}
