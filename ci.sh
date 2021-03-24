eda07a4e7dfddbdaafe56d3087ddc0dc5ada88e2
Atila Neves <atila.neves@gmail.com>
2021-03-24 08:08:34 -0300

    Fix example tests

--- ci.sh
+++ ci.sh
@@ -9,10 +9,15 @@ DC="${DC:-dmd}"
 echo "dub build type: $DUB_BUILD_TYPE"
 echo "DC: $DC"
 
+echo "Unit tests"
 dub test -q --build="$DUB_BUILD_TYPE" --compiler="$DC"
+
+echo "Build basic example"
 cd "$SCRIPT_DIR"/example/basic
 dub build -q
 
 cd "$SCRIPT_DIR"/example/widgets
+echo "Test widget example"
 dub test -q --build="$DUB_BUILD_TYPE" --compiler="$DC"
+echo "Build widget example"
 dub build -q
